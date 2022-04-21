c0cf407eda1e1b4bcc40af25e77b43c98857c4d9
Muneerah1612 <muniratolayiwola@gmail.com>
2022-04-21 17:44:18 +0100

    refactor code

--- mfaapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-
-
-class TxSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['rpc-user','rpc-pass','txid','rpc-port']
--- mfaapi/views.py
+++ mfaapi/views.py
@@ -1,70 +1,96 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import authentication, permissions,status
+from rest_framework import status
 from .utils import CheckNodeConnection
-from .serializers import TxSerializer
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-# from decimal import Decimal
-import json
-
 from rest_framework.response import Response
 
-# Create your views here.
-class GetTxDetails(APIView):
-    def post(self,request):
-        rpc_user=request.data['rpc-user']
-        rpc_pwd=request.data['rpc-pass']
-        network=request.data['rpc_port']
-        rpc_connect=CheckNodeConnection(network,rpc_user,rpc_pwd)
-        mempool_list = rpc_connect.getrawmempool()
-        # print(mempool_list==True)
-        txid=request.data['txid']
-        if txid in mempool_list:
-            txinfo=rpc_connect.getmempoolentry(txid)
-            print(txinfo['fees']['base'])
-            return Response({'status':status.HTTP_302_FOUND, 'message':txinfo})
-        else:
-            return Response({'status':status.HTTP_404_NOT_FOUND, 'message':'Transaction not in mempool'})
 
-## restructure the code to take in the rpc user,rpc password and network type (mainnet,signet,testnet)
+class GetTxDetails(APIView):
+    def post(self, request):
+        rpc_user = request.data["rpc_user"]
+        rpc_pwd = request.data["rpc_pwd"]
+        network = request.data["rpc_port"]
+        rpc_connect = CheckNodeConnection(network, rpc_user, rpc_pwd)
+        mempool_list = rpc_connect.getrawmempool()
+
+        txid = request.data["txid"]
+        if txid in mempool_list:
+            txinfo = rpc_connect.getmempoolentry(txid)
+            return Response({"status": status.HTTP_302_FOUND, "message": txinfo})
+        else:
+            return Response(
+                {
+                    "status": status.HTTP_404_NOT_FOUND,
+                    "message": "Transaction not in mempool",
+                }
+            )
+
+
 class ListFeesView(APIView):
-    def post(self,request):
-        rpc_user=request.data['rpc-user']
-        rpc_pwd=request.data['rpc-pass']
-        network=request.data['rpc-port']
+    def post(self, request):
+        rpc_user = request.data["rpc_user"]
+        rpc_pwd = request.data["rpc_pwd"]
+        network = request.data["rpc_port"]
         try:
-            rpc_connect=CheckNodeConnection(network,rpc_user,rpc_pwd)
-            #rpc_connection = AuthServiceProxy("http://%s:%s@localhost:18443"%(rpc_user, rpc_pwd))
-            mempool_list = rpc_connect.getrawmempool()
-            details=[{i:rpc_connect.getmempoolentry(i)['fees']['base']} for i in mempool_list]
-            return Response({'status':status.HTTP_302_FOUND, 'message':details})
+            rpc_connect = CheckNodeConnection(network, rpc_user, rpc_pwd)
+            mempool_list = rpc_connect.getrawmempool(True)
+            details = [{i: str(mempool_list[i]["fees"]["base"])} for i in mempool_list]
+            return Response({"status": status.HTTP_302_FOUND, "message": details})
         except ConnectionRefusedError:
-            return Response({'status':status.HTTP_404_NOT_FOUND, 'message':'Check your rpc username/password, and ensure bitcon core is running'})
+            return Response(
+                {
+                    "message": "Check your rpc username/password, and ensure bitcon core is running"
+                }
+            )
 
 
 class HighLowFeesView(APIView):
-    def post(self,request):
-        rpc_user=request.data['rpc-user']
-        rpc_pwd=request.data['rpc-pass']
-        network=request.data['rpc-port']
-        rpc_connect=CheckNodeConnection(network,rpc_user,rpc_pwd)
-        # rpc_connection = AuthServiceProxy("http://%s:%s@localhost:18443"%(rpc_user, rpc_pwd))
-        mempool_list = rpc_connect.getrawmempool()
-        details=[rpc_connect.getmempoolentry(i)['fees']['base'] for i in mempool_list]
-        highlow= [{'lowest fee':min(details)},{'highest fee':max(details)}]
-        return Response({'status':status.HTTP_302_FOUND, 'message':highlow})
+    def post(self, request):
+        rpc_user = request.data["rpc_user"]
+        rpc_pwd = request.data["rpc_pwd"]
+        network = request.data["rpc_port"]
+        try:
+            rpc_connect = CheckNodeConnection(network, rpc_user, rpc_pwd)
+            mempool_list = rpc_connect.getrawmempool(True)
+            details = [mempool_list(i)["fees"]["base"] for i in mempool_list]
+            highlow = [
+                {"lowest fee": str(min(details))},
+                {"highest fee": str(max(details))},
+            ]
+            return Response({"status": status.HTTP_302_FOUND, "message": highlow})
+        except ConnectionRefusedError:
+            return Response(
+                {
+                    "message": "Check your rpc username/password, and ensure bitcon core is running"
+                }
+            )
+
 
 class GetMempoolLastTransaction(APIView):
-    def post(self,request):
-        rpc_user=request.data['rpc-user']
-        rpc_pwd=request.data['rpc-pass']
-        network=request.data['rpc-port']
-        rpc_connection = CheckNodeConnection(network,rpc_user,rpc_pwd)
-        mempool_list = rpc_connection.getrawmempool(True)
-        details=[mempool_list[i]['time'] for i in mempool_list]
-        l=[{i:{'time':mempool_list[i]['time'],'fees':mempool_list[i]['fees'],'vsize':mempool_list[i]['vsize']}} for i in mempool_list if mempool_list[i]['time']==max(details)]
-        return Response({'status':status.HTTP_302_FOUND, 'message':l})
-
-
-        
-
+    def post(self, request):
+        rpc_user = request.data["rpc_user"]
+        rpc_pwd = request.data["rpc_pwd"]
+        network = request.data["rpc_port"]
+        try:
+            rpc_connection = CheckNodeConnection(network, rpc_user, rpc_pwd)
+            mempool_list = rpc_connection.getrawmempool(True)
+            details = [mempool_list[i]["time"] for i in mempool_list]
+            lasttxinfo = [
+                {
+                    i: {
+                        "time": mempool_list[i]["time"],
+                        "fees": str(mempool_list[i]["fees"]["base"]),
+                        "vsize": mempool_list[i]["vsize"],
+                    }
+                }
+                for i in mempool_list
+                if mempool_list[i]["time"] == max(details)
+            ]
+            return Response({"status": status.HTTP_302_FOUND, "message": lasttxinfo})
+        except ConnectionRefusedError:
+            return Response(
+                {
+                    "message": "Check your rpc username/password, and ensure bitcon core is running"
+                }
+            )
